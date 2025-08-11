157e14ae9c70956802237f316194bb6915031f92
Parna Das <dparna28@gmail.com>
2025-08-11 11:36:11 +0530

    new commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     const {selectedUser} = useContext(ChatContext)
     return (
         <div className='border w-full h-screen sm:px-[15%] sm:py[5%]'>
-            <div className={`backdrop-blur-xl border-2 border-purple-700 rounded-2xl overflow-hidden h-[85%] grid grid-cols-1 relative mt-[55px] ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+            <div className={`backdrop-blur-xl border-2 border-purple-900 rounded-2xl overflow-hidden h-[90%] mt-[30px] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
                 <Sidebar />
                 <ChatContainer />
                 <RightSidebar />
